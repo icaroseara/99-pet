a1fec5669cb8596b76a6107c57f86fe76d2b8d25
Icaro Seara <ikro89@gmail.com>
2017-12-16 22:43:38 -0200

    Add update action at driver's controller

--- app/controllers/v1/DriverController.scala
+++ app/controllers/v1/DriverController.scala
@@ -5,15 +5,14 @@ import javax.inject.Inject
 import play.api.mvc._
 import play.api.Logger
 import play.api.libs.json.Json
+
+import helpers.MongoExceptionHandler
 import services.DriverService
 import models.Driver
-import helpers.MongoExceptionHandler
-import reactivemongo.bson.BSONObjectID
-
 
 import scala.concurrent._
+import reactivemongo.bson.BSONObjectID
 import ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class DriverController @Inject()(cc: ControllerComponents, driverService: DriverService)
   extends AbstractController(cc)
@@ -26,16 +25,32 @@ class DriverController @Inject()(cc: ControllerComponents, driverService: Driver
       driverService.create(driver).map{ _ =>
         Created
       }
-    }.getOrElse(Future.successful(BadRequest("Invalid driver format")))
+    }.getOrElse{
+      logger.info(s"Cannot create a driver with invalid info")
+      Future.successful(BadRequest("Invalid driver format"))
+    }
   }
 
   def read(id: BSONObjectID) = Action.async{ req =>
     driverService.find(id).map{ maybeDriver =>
       maybeDriver.map{ driver =>
         Ok(Json.toJson(driver))
-      }.getOrElse(NotFound)
+      }.getOrElse{
+        logger.info(s"Cannot find a driver with: id=$id")
+        NotFound(s"Cannot find a user with id=$id")
+      }
     }
   }
 
-  def update(id: BSONObjectID) = TODO
+  def update(id: BSONObjectID) =  Action.async(parse.json){ req =>
+    req.body.validate[Driver].map{ driver =>
+      driverService.update(id, driver).map {
+        case Some(driver) => Ok(Json.toJson(driver))
+        case None => NotFound
+      }
+    }.getOrElse{
+      logger.info(s"Cannot update a driver invalid info")
+      Future.successful(BadRequest("Invalid request body"))
+    }
+  }
 }
