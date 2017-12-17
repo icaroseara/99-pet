c435976dc85970d9a35e489263e4908598021339
Icaro Seara <ikro89@gmail.com>
2017-12-16 22:21:37 -0200

    Improved controllers

--- app/controllers/v1/PetssengerController.scala
+++ app/controllers/v1/PetssengerController.scala
@@ -2,13 +2,20 @@ package controllers.v1
 
 import javax.inject.Inject
 
+import helpers.MongoExceptionHandler
+import play.api.Logger
 import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
 
-class PetssengerController @Inject()() extends Controller {
+class PetssengerController @Inject()(cc: ControllerComponents)
+    extends AbstractController(cc)
+      with MongoExceptionHandler  {
+
+  private val logger = Logger(this.getClass)
 
   def create = TODO
 
-  def read(id: String) = TODO
+  def read(id: BSONObjectID) = TODO
 
-  def update(id: String) = TODO
+  def update(id: BSONObjectID) = TODO
 }
--- app/controllers/v1/RideController.scala
+++ app/controllers/v1/RideController.scala
@@ -2,13 +2,20 @@ package controllers.v1
 
 import javax.inject.Inject
 
+import helpers.MongoExceptionHandler
+import play.api.Logger
 import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
 
-class RideController @Inject()() extends Controller {
+class RideController @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc)
+    with MongoExceptionHandler  {
+
+  private val logger = Logger(this.getClass)
 
   def start = TODO
 
-  def status(id: String) = TODO
+  def status(id: BSONObjectID) = TODO
 
-  def notification(id: String) = TODO
+  def notification(id: BSONObjectID) = TODO
 }
