d08496307d796fe1109c3b351593bc505b60f18d
Icaro Seara <ikro89@gmail.com>
2017-12-16 23:00:21 -0200

    Add petssenger repository

---
+++ app/repositories/PetssengerRepository.scala
@@ -0,0 +1,51 @@
+package repositories
+
+import javax.inject.Inject
+
+import models.Petssenger
+import models.Petssenger._
+
+import com.google.inject.ImplementedBy
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection.JSONCollection
+import play.modules.reactivemongo.ReactiveMongoApi
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[PetssengerRepositoryImpl])
+trait PetssengerRepository {
+  def find(id: BSONObjectID): Future[Option[Petssenger]]
+
+  def create(petssenger: Petssenger): Future[WriteResult]
+
+  def update(id: BSONObjectID, petssenger: Petssenger): Future[Option[Petssenger]]
+}
+
+class PetssengerRepositoryImpl @Inject()(implicit ec: ExecutionContext, reactiveMongoApi: ReactiveMongoApi)
+  extends PetssengerRepository {
+
+  def petssengersFuture: Future[JSONCollection] = reactiveMongoApi.database.map(_.collection("petssengers"))
+
+  override def find(id: BSONObjectID): Future[Option[Petssenger]] = {
+    val query = BSONDocument("_id" -> id)
+    petssengersFuture.flatMap(_.find(query).one[Petssenger])
+  }
+
+  override def create(petssenger: Petssenger): Future[WriteResult] = {
+    petssengersFuture.flatMap(_.insert(petssenger))
+  }
+
+  override def update(id: BSONObjectID, petssenger: Petssenger): Future[Option[Petssenger]] = {
+    val selector = BSONDocument("_id" -> id)
+    val updateModifier = BSONDocument(
+      "$set" -> BSONDocument(
+        "email" -> petssenger.email
+      )
+    )
+
+    petssengersFuture.flatMap(
+      _.findAndUpdate(selector, updateModifier, fetchNewObject = true).map(_.result[Petssenger])
+    )
+  }
+}
