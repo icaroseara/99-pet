bf16967fa5852a499a3e513e9b3122c01aec7835
Icaro Seara <ikro89@gmail.com>
2017-12-16 22:59:52 -0200

    Add petssenger format to its model

--- app/models/Petssenger.scala
+++ app/models/Petssenger.scala
@@ -1,5 +1,14 @@
 package models
 
 import java.util.UUID
+import play.api.libs.json._
 
-case class Petssenger(id: UUID, name: String, email: String)
+case class Petssenger(
+                   id: UUID,
+                   name: String,
+                   email: String
+                 )
+
+object Petssenger {
+  implicit val petssengerFormat: OFormat[Petssenger] = Json.format[Petssenger]
+}
