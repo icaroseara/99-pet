a6bc2453d4685180809fa7409ce456aeb91397b4
Icaro Seara <ikro89@gmail.com>
2017-12-16 23:01:05 -0200

    Add petssenger actions to its controller

--- app/controllers/v1/PetssengerController.scala
+++ app/controllers/v1/PetssengerController.scala
@@ -2,20 +2,55 @@ package controllers.v1
 
 import javax.inject.Inject
 
-import helpers.MongoExceptionHandler
-import play.api.Logger
 import play.api.mvc._
-import reactivemongo.bson.BSONObjectID
+import play.api.Logger
+import play.api.libs.json.Json
 
-class PetssengerController @Inject()(cc: ControllerComponents)
-    extends AbstractController(cc)
-      with MongoExceptionHandler  {
+import helpers.MongoExceptionHandler
+import services.PetssengerService
+import models.Petssenger
+
+import scala.concurrent._
+import reactivemongo.bson.BSONObjectID
+import ExecutionContext.Implicits.global
+
+class PetssengerController @Inject()(cc: ControllerComponents, petssengerService: PetssengerService)
+  extends AbstractController(cc)
+    with MongoExceptionHandler  {
 
   private val logger = Logger(this.getClass)
 
-  def create = TODO
+  def create = Action.async(parse.json) { request =>
+    request.body.validate[Petssenger].map{ petssenger =>
+      petssengerService.create(petssenger).map{ _ =>
+        Created
+      }
+    }.getOrElse{
+      logger.info(s"Cannot create a petssenger with invalid info")
+      Future.successful(BadRequest("Invalid petssenger format"))
+    }
+  }
 
-  def read(id: BSONObjectID) = TODO
+  def read(id: BSONObjectID) = Action.async{ req =>
+    petssengerService.find(id).map{ maybePetssenger =>
+      maybePetssenger.map{ petssenger =>
+        Ok(Json.toJson(petssenger))
+      }.getOrElse{
+        logger.info(s"Cannot find a petssenger with: id=$id")
+        NotFound(s"Cannot find a user with id=$id")
+      }
+    }
+  }
 
-  def update(id: BSONObjectID) = TODO
+  def update(id: BSONObjectID) =  Action.async(parse.json){ req =>
+    req.body.validate[Petssenger].map{ petssenger =>
+      petssengerService.update(id, petssenger).map {
+        case Some(petssenger) => Ok(Json.toJson(petssenger))
+        case None => NotFound
+      }
+    }.getOrElse{
+      logger.info(s"Cannot update a petssenger invalid info")
+      Future.successful(BadRequest("Invalid request body"))
+    }
+  }
 }
