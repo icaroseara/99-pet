d18392f6a1da1a73a13b6252b98e0a2d5bcbffac
Icaro Seara <icaro.seara@gmail.com>
2017-12-18 22:37:20 -0200

    Add find petssenger specs

--- app/models/Petssenger.scala
+++ app/models/Petssenger.scala
@@ -1,13 +1,11 @@
 package models
 
-import java.util.UUID
+import reactivemongo.bson.BSONObjectID
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection.JSONCollection
 import play.api.libs.json._
 
-case class Petssenger(
-                   id: UUID,
-                   name: String,
-                   email: String
-                 )
+case class Petssenger(id: Option[BSONObjectID], name: String, email: String)
 
 object Petssenger {
   implicit val petssengerFormat: OFormat[Petssenger] = Json.format[Petssenger]
--- test/repositories/PetssengerRepositorySpec.scala
+++ test/repositories/PetssengerRepositorySpec.scala
@@ -1,11 +1,52 @@
-package repositories
+package test
 
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import play.api.test._
+import javax.inject.Inject
+
+import models.Petssenger
+import repositories.PetssengerRepository
+import org.scalatest.BeforeAndAfter
+import play.api.libs.json.Json
 import play.api.test.Helpers._
+import reactivemongo.bson.BSONDocument
+import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.play.json._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class PetssengerRepositorySpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class PetssengerRepositorySpec @Inject()(petssengerRepository: PetssengerRepository) extends PlayWithMongoSpec with BeforeAndAfter {
   "PetssengerRepository respository" should {
+    var petssengers: Future[JSONCollection] = null.asInstanceOf[Future[JSONCollection]]
+
+    before {
+      //Init DB
+      await {
+        petssengers = reactiveMongoApi.database.map(_.collection("petssengers"))
+
+        val petssenger = Petssenger(None, "Buda", "duda@email.com")
+
+        petssengers.flatMap(_.bulkInsert(ordered = false)(
+          petssenger
+        ))
+      }
+    }
+
+    after {
+      //clean DB
+      petssengers.flatMap(_.drop(failIfNotFound = false))
+    }
+
+
+    "find a petssenger" in {
+      val query = BSONDocument()
+      val Some(petssengerToFind) = await(petssengers.flatMap(_.find(query).one[Petssenger]))
+      val pessengerIdToFind = petssengerToFind.id.get
+      petssengerRepository.find(pessengerIdToFind) mustBe Petssenger
+    }
+    "create a petseenger" in {
+
+    }
+    "update a petssenger" in {
+
+    }
   }
 }
